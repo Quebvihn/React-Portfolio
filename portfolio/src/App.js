8bafa0c725dde835f9e3a967316dcbfb138a6f28
Quebvihn <bamedinatorres@gmail.com>
2023-12-02 11:54:56 -0500

    app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,21 @@
-
 import './App.css';
+import styled, {ThemeProvider} from'styled-components';
+import { darkTheme } from './utils/theme';
+
+const Body = styled.div`
+background-color: ${({ theme }) => theme.bg};
+width: 100%;
+height: 100%;
+overflow-x: hidden;
+`;
 
 function App() {
   return (
-    <div className="App">
-        Portfolio
-    </div>
-  );
+    <ThemeProvider theme={darkTheme}>
+      <Body>Portfolio</Body>
+    </ThemeProvider>
+        
+  )
 }
 
 export default App;
